d3fedc859c00a62cda9ed73e4161342bf460dcfc
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-09-19 15:17:51 +0200

    player move con addforce

--- Assets/Scripts/Player/SimpleMovement.cs
+++ Assets/Scripts/Player/SimpleMovement.cs
@@ -9,6 +9,11 @@ namespace Player
 
         [SerializeField] private Rigidbody2D rb2d;
         [SerializeField] private float speed;
+        [SerializeField] private float runAccelAmount;
+        [SerializeField] private float runDeccelAmount;
+
+        [Header("Test move with force")]
+        public bool testMoveImprove;
 
         // Update is called once per frame
         void Update()
@@ -18,18 +23,43 @@ namespace Player
 
         private void FixedUpdate()
         {
-            PlayerMovement(speed);
+            if (testMoveImprove)
+            {
+                PlayerMoveImprove();
+            }
+            else
+            {
+                PlayerMovement(speed);
+            }
         }
 
         private void PlayerInputsValues()
         {
-            movement.x = Input.GetAxis("Horizontal");
-            movement.y = Input.GetAxis("Vertical");
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
         }
 
         private void PlayerMovement(float s)
         {
-            rb2d.MovePosition(rb2d.position + movement * (s * Time.fixedDeltaTime));
+            if (movement.x == 0 || movement.y == 0)
+            {
+                rb2d.velocity = Vector2.zero;
+            }
+            
+            rb2d.MovePosition(rb2d.position + movement.normalized * (s * Time.fixedDeltaTime));
         }
+
+        private void PlayerMoveImprove()
+        {
+            Vector2 targetSpeed = new Vector2(movement.x, movement.y) * speed;
+            Vector2 accelRate = new Vector2((Mathf.Abs(targetSpeed.x) > 0.01f) ? runAccelAmount : runDeccelAmount,
+                (Mathf.Abs(targetSpeed.y) > 0.01f) ? runAccelAmount : runDeccelAmount);
+            Vector2 speedDif = new Vector2(targetSpeed.x - rb2d.velocity.x, targetSpeed.y - rb2d.velocity.y);
+            Vector2 rate = new Vector2(speedDif.x * accelRate.x, speedDif.y * accelRate.y);
+            
+            rb2d.AddForce(rate, ForceMode2D.Force);
+        }
+
+        
     }
 }
