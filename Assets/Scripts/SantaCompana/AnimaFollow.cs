4851d2edd66962eda3ec5ea623c687c349e2f6ee
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-09-22 18:40:52 +0200

    anima prefabs

--- Assets/Scripts/SantaCompana/AnimaFollow.cs
+++ Assets/Scripts/SantaCompana/AnimaFollow.cs
@@ -8,16 +8,23 @@ namespace SantaCompana
     {
 
         [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private NavMeshAgent destinyAgent;
         [SerializeField] private Transform destination;
+
+        [SerializeField] private Animator animator;
+        [SerializeField] private string animName;
 
         private void Start()
         {
             agent.updateRotation = false;
             agent.updateUpAxis = false;
+            
+            animator.Play(animName);
         }
 
         private void Update()
         {
+            agent.speed = destinyAgent.speed;
             agent.SetDestination(destination.position);
         }
     }
