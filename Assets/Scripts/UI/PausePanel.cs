73e7c1010a23da045a88cd4853c318221b3e1b90
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-09-22 19:15:52 +0200

    Menu pausa chustero

--- Assets/Scripts/UI/PausePanel.cs
+++ Assets/Scripts/UI/PausePanel.cs
@@ -1,9 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace UI
 {
     public class PausePanel : MonoBehaviour
     {
-        
+        private void OnEnable()
+        {
+            Time.timeScale = 0;
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1;
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
