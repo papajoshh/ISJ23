913b66b9636be274742257336fa43a4cbf2a6a28
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-09-23 17:28:17 +0200

    camera effects

--- Assets/Scripts/Camera/CameraEffectController.cs
+++ Assets/Scripts/Camera/CameraEffectController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Cinemachine;
 using UnityEngine;
 
@@ -10,6 +11,11 @@ namespace Camera
         [SerializeField] private CinemachineVirtualCamera virtualCamera;
         private CinemachineBasicMultiChannelPerlin effect;
 
+        private bool enemyIsInRange;
+
+        private float initValue = 3.25f;
+        private float zoomValue = 3f;
+
         private void Start()
         {
             effect = virtualCamera.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
@@ -17,8 +23,48 @@ namespace Camera
 
         private void Update()
         {
-            effect.m_AmplitudeGain = 1f;
-            effect.m_FrequencyGain = 1f;
+            if(enemyIsInRange && zoomValue < virtualCamera.m_Lens.OrthographicSize)
+            {
+                virtualCamera.m_Lens.OrthographicSize -= Time.deltaTime / 1.5f;
+            }
+            else if (!enemyIsInRange && virtualCamera.m_Lens.OrthographicSize < initValue)
+            {
+                virtualCamera.m_Lens.OrthographicSize += Time.deltaTime;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("Enemy"))
+            {
+                enemyIsInRange = true;
+                StopAllCoroutines();
+                StartCoroutine(StartEffectCorroutine(1f, 0f, 1f));
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                enemyIsInRange = false;
+                StopAllCoroutines();
+                StartCoroutine(StartEffectCorroutine(1f, 1f, 0f));
+            }
+        }
+
+
+        IEnumerator StartEffectCorroutine(float duration, float start, float targetValue)
+        {
+            float currentTime = 0;
+            while (currentTime < duration)
+            {
+                currentTime += Time.deltaTime;
+                effect.m_AmplitudeGain = Mathf.Lerp(start, targetValue, currentTime / duration);
+                effect.m_FrequencyGain = Mathf.Lerp(start, targetValue, currentTime / duration);
+                yield return null;
+            }
+            yield break;
         }
     }
 }
