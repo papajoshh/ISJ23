f00cf554dad0acd1d02821651b41389b31b21ccb
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-09-23 14:23:51 +0200

    MusicBGController

--- Assets/Scripts/Music/AmbienSounds.cs
+++ Assets/Scripts/Music/AmbienSounds.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Music
@@ -6,16 +7,51 @@ namespace Music
     public class AmbienSounds : MonoBehaviour
     {
 
-        [SerializeField] private AudioSource audioSource;
-        
+        [SerializeField] private AudioSource audioSourceDay;
+        [SerializeField] private AudioSource audioSourceNigth;
+
         private void OnEnable()
         {
-            audioSource.Play();
+            StartCoroutine(StartFade(audioSourceDay, 2f, 0.2f));
         }
 
         private void OnDisable()
         {
-            audioSource.Stop();
+            audioSourceDay.Stop();
         }
+
+        private IEnumerator StartFade(AudioSource audioS, float duration, float targetVolume)
+        {
+            audioS.Play();
+            float currentTime = 0;
+            float start = audioS.volume;
+            while (currentTime < duration)
+            {
+                currentTime += Time.deltaTime;
+                audioS.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+                yield return null;
+            }
+            yield break;
+        }
+
+        private IEnumerator FadeOut(AudioSource audioS, float duration, float targetVolume)
+        {
+            float currentTime = 0;
+            float start = audioS.volume;
+            while (currentTime < duration)
+            {
+                currentTime += Time.deltaTime;
+                audioS.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+                yield return null;
+            }
+            yield break;
+        }
+        
+        public void ChangeMusic()
+        {
+            StartCoroutine(FadeOut(audioSourceDay, 3f, 0f));
+            StartCoroutine(StartFade(audioSourceNigth, 4f, 0.15f));
+        }
+        
     }
 }
