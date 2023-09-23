b0d0c665e442995a57e0b440b0921d76f8d7fe7f
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-09-23 23:24:23 +0200

    Peregrino animaciones y sprites

--- Assets/Scripts/General/SimpleDialogInteractor.cs
+++ Assets/Scripts/General/SimpleDialogInteractor.cs
@@ -7,9 +7,12 @@ public class SimpleDialogInteractor : MonoBehaviour, IInteractable
     [Header("[Configuration]")]
     [SerializeField] private List<DialogScriptable> dialogList;
 
+    [SerializeField] private Animator animator;
+
 
     public void Interact()
     {
+        animator.Play("Talk");
         UI_DialogPanel.instance.onEndDialog += OnEndDialog;
 
         Core.GameStateController.Instance.ChangeGameStateTo(Core.GameStateController.GameState.Pause);
@@ -18,6 +21,7 @@ public class SimpleDialogInteractor : MonoBehaviour, IInteractable
 
     private void OnEndDialog()
     {
+        animator.Play("Idle");
         UI_DialogPanel.instance.onEndDialog -= OnEndDialog;
         Core.GameStateController.Instance.ChangeGameStateTo(Core.GameStateController.GameState.Gameplay);
         Player_Interactor.instance.EnableInteracting();
