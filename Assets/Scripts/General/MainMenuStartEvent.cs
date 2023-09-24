2b5eaab00dcaaeaebc91e5ae3f3ff3dd3ca883e6
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2023-09-24 21:49:08 +0200

    Main menu animation

---
+++ Assets/Scripts/General/MainMenuStartEvent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuStartEvent : MonoBehaviour
+{
+    [Header("[References]")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Animator startAnimator;
+
+    [Header("[Sounds]")]
+    [SerializeField] private AudioClip thunderSFX;
+
+
+    public void Play_StartAnimation()
+    {
+        startAnimator.Play("START");
+    }
+
+    public void Play_ThunderSFX()
+    {
+        audioSource.PlayOneShot(thunderSFX);
+    }
+
+    public void LoadGameplay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+}
