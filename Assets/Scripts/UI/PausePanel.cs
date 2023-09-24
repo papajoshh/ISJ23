06e8bc240790f7f22c7a02b22ebb61241402417d
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2023-09-24 17:04:05 +0200

    Audiomixer main menu y pausa

--- Assets/Scripts/UI/PausePanel.cs
+++ Assets/Scripts/UI/PausePanel.cs
@@ -1,19 +1,39 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class PausePanel : MonoBehaviour
     {
+        [Header("[References]")]
+        [SerializeField] private AudioMixer audiomixer;
+        [SerializeField] private Slider volumeSlider;
+
         private void OnEnable()
         {
+            audiomixer.GetFloat("Master", out float currentVolume);
+            volumeSlider.value = Mathf.Pow(10, currentVolume / 20);
+
+            volumeSlider.onValueChanged.AddListener(SetMusicVolume);
+
             Time.timeScale = 0;
         }
 
         private void OnDisable()
         {
+            volumeSlider.onValueChanged.RemoveListener(SetMusicVolume);
             Time.timeScale = 1;
+        }
+
+        private void SetMusicVolume(float sliderValue)
+        {
+            audiomixer.SetFloat("Master", Mathf.Log10(sliderValue) * 20);
+
+            if (sliderValue <= 0)
+                audiomixer.SetFloat("Master", -80);
         }
 
         public void OnClick_Resume()
