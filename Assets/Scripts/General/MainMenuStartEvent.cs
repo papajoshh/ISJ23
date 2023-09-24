fd5c8f504e1eabea779ba2ba6951514098b1e738
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2023-09-25 00:01:05 +0200

    Animacion main menu update

--- Assets/Scripts/General/MainMenuStartEvent.cs
+++ Assets/Scripts/General/MainMenuStartEvent.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class MainMenuStartEvent : MonoBehaviour
 {
     [Header("[References]")]
+    [SerializeField] private AudioSource audiosourceMusica;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private Animator startAnimator;
 
     [Header("[Sounds]")]
     [SerializeField] private AudioClip thunderSFX;
+    [SerializeField] private AudioClip bellSFX;
 
 
     public void Play_StartAnimation()
@@ -20,7 +22,9 @@ public class MainMenuStartEvent : MonoBehaviour
 
     public void Play_ThunderSFX()
     {
+        audiosourceMusica.Stop();
         audioSource.PlayOneShot(thunderSFX);
+        audioSource.PlayOneShot(bellSFX);
     }
 
     public void LoadGameplay()
